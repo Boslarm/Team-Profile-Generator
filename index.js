b3843e9b4f17f720d924d0cc616396ac9c3d1e76
Jordan <jordantylerharris@gmail.com>
2022-09-26 20:19:50 -0400

    installed inquirer & jest

--- index.js
+++ index.js
@@ -2,3 +2,7 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 const inquirer = require('inquirer');
 
+const Manager = require('./lib/Manager');
+const Engineer = require('./lib/Engineer');
+const Intern = require('./lib/Intern');
+
