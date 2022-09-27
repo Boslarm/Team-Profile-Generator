8a1b857f42a6543e9889cbcd821a3fd4ada813b9
Jordan <jordantylerharris@gmail.com>
2022-09-27 19:15:40 -0400

    have looping function in, now to write html

--- index.js
+++ index.js
@@ -64,7 +64,7 @@ function init() {
     newEmployee()
 }
 function newEmployee() {
-    inquirer.prompt(questions).then((response) => {
+    inquirer.prompt(questions).then(async (response) => { //have to use async to allow for all prompts before moving on to creating
         let name = response.name;
         let id = response.id;
         let email = response.email;
@@ -74,26 +74,51 @@ function newEmployee() {
         let school;
 
         if (role === 'Manager') {
-            inquirer.prompt(managerQuestions).then((response) => { //prompts manager question
+            await inquirer.prompt(managerQuestions).then((response) => { //prompts manager question
                 officeNumber = response.officeNumber;
                 let employee = new Manager(name, id, email, officeNumber); //adds employee as manager
                 employeeArr.push(employee); //pushes data to array for export
             });
         }
         else if (role === 'Engineer') {
-            inquirer.prompt(engineerQuestions).then((response) => {
+            await inquirer.prompt(engineerQuestions).then((response) => {
                 gitHub = response.gitHub;
                 let employee = new Engineer(name, id, email, gitHub);
                 employeeArr.push(employee);
             });
         }
         else if (role === 'Intern') {
-            inquirer.prompt(internQuestions).then((response) => {
+            await inquirer.prompt(internQuestions).then((response) => {
                 school = response.school;
                 let employee = new Intern(name, id, email, school);
                 employeeArr.push(employee);
             });
+        } addEmployee() //after all questions are asked call function to add another employee
+    } 
+    )
+};
+//add function to ask if another employee is to be added
+function addEmployee() {
+    inquirer.prompt(
+        {
+            type: 'confirm',
+            name: 'addEmployee',
+            message: "Would you like to add another employee?"
         }
-    }
-)}
+    ).then((response) => {
+        let createEmployee = response.addEmployee;
+        if (createEmployee === true) {
+            newEmployee(); //loop back to newEployee function to continue
+        } 
+        else if (createEmployee === false) {
+            console.log("Success! Your team profile has been created. View it in the 'dist' folder.")
+            //write/create the file
+            fs.writeFile('./dist/index.html', err => {
+                if (err) {
+                    console.log(err)
+                }
+            })
+        }
+    })
+};
 init();
